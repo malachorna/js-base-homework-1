4a6a18753e3b9fffa7db3ead362500ddaa005036
Iryna Prytula <96309174+malachorna@users.noreply.github.com>
2022-02-14 00:07:51 +0200

    Притула Ірина

---
+++ script.js
@@ -0,0 +1,18 @@
+const city = "Kyiv"
+city = "Lviv"
+const address = "Zhovkva"
+address = city
+
+console.log("address")
+
+
+let n = 18
+let result = ("x-18 y-3")
+console.log("n * 3")
+
+
+const pen = 4
+const pensil = 6
+const number1 = 5.25
+const number2 = 3.5
+const sum = ("x-5.25 *y-4" + "x-3.5*y-6")
